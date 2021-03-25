1300114f3646ac0076e7578d7183c2f3e6899516
Pedro Coelho <79021161+pedrocoelho94@users.noreply.github.com>
2021-03-25 17:08:15 -0300

    adicionada a opção de fechar a mensagem após uma ação

--- aula4/js/script.js
+++ aula4/js/script.js
@@ -1,6 +1,7 @@
 const catalogo = document.querySelector('#catalogo')
 const msg = document.querySelector('.operacao')
 
+
 let filmes = ['inception', 'sing street', 'whiplash', 'hamilton', 'ready player one', 'promising young woman']
 
 let listaImgFilmes = ['https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg',
@@ -12,13 +13,14 @@ function exibeFilmes(){
         catalogo.innerHTML += `
         <div class='singleFilm'>
         <img src="${listaImgFilmes[i]}">
-        <span>${filmes[i]}</span>
+        <span class='nomeFilme'>${filmes[i]}</span>
         </div>
         `
     }
 }
 
 exibeFilmes()
+
 
 function addFilme(){
         
@@ -29,35 +31,49 @@ function addFilme(){
     if(newFilme == '' || newImgFilme == ''){
         alert('Um dos campos está vázio!')
     }else if(newFilme == null || newImgFilme == null){
-        msg.innerHTML = 'Operação Cancelada'
+        msg.innerHTML = 'Operação Cancelada<span onclick="fecharMensagem()" class="close" title="Fechar">x</span>'
+        mostrarMensagem()
     }else{
         filmes.push(newFilme)
         listaImgFilmes.push(newImgFilme)
         catalogo.innerHTML += `
         <div class='singleFilm'>
-        <img src="${listaImgFilmes[listaImgFilmes.length-1]}">
-        <span>${filmes[filmes.length-1]}</span>
+        <img onclick="trailer()" src="${listaImgFilmes[listaImgFilmes.length-1]}">
+        <span class='nomeFilme'>${filmes[filmes.length-1]}</span>
         </div>
         `
-        msg.innerHTML = 'Filme Adicionado com Sucesso'
+        msg.innerHTML = 'Filme Adicionado com Sucesso<span onclick="fecharMensagem()" class="close" title="Fechar">x</span>'
+        mostrarMensagem()
     }
     
 }
+
 
 function removerFilme(){
     let deleteFilme = prompt('Qual filme deseja remover?')
     deleteFilme = deleteFilme.toLowerCase()
 
     if(filmes.indexOf(deleteFilme) == -1){
-        msg.innerHTML = 'Filme Não Encontrado'
+        msg.innerHTML = 'Filme Não Encontrado<span onclick="fecharMensagem()" class="close" title="Fechar">x</span>'
+        mostrarMensagem()
     }else{
         deleteIndice = filmes.indexOf(deleteFilme)
         filmes.splice(deleteIndice,1)
         listaImgFilmes.splice(deleteIndice,1)
         catalogo.innerHTML = ''
         exibeFilmes()
-        msg.innerHTML = 'Filme Removido com Sucesso'
+        msg.innerHTML = 'Filme Removido com Sucesso<span onclick="fecharMensagem()" class="close" title="Fechar">x</span>'
+        mostrarMensagem()
+       
     }
-    
+  
 }
+function fecharMensagem(){
+    document.getElementById("msg").style.display = 'none'; 
+}
+
+function mostrarMensagem(){
+    document.getElementById("msg").style.display = 'block'; 
+}
+
 
