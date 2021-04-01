baa3bf98a03321a8e94499bbba652b360ef40f42
Pedro Coelho <79021161+pedrocoelho94@users.noreply.github.com>
2021-04-01 18:25:04 -0300

    new features

--- aula7/js/script.js
+++ aula7/js/script.js
@@ -2,27 +2,27 @@ let carta1 = {
     nome: 'Katarina',
     foto: 'https://i.imgur.com/cj9WPpm.jpg',
     atributos:{
-        ataque: 60,
-        defesa: 30,
-        magia: 70
+        ataque: 62,
+        defesa: 33,
+        magia: 73
     }    
 }
 let carta2 = {
     nome: 'Lux',
     foto: 'https://i.imgur.com/d5hmBCd.jpg',
     atributos:{
-        ataque: 40,
-        defesa: 50,
-        magia: 75
+        ataque: 35,
+        defesa: 38,
+        magia: 76
     } 
 }
 let carta3 = {
     nome: 'Caitlyn',
     foto: 'https://p2.trrsf.com/image/fget/cf/460/0/images.terra.com/2021/03/04/51022-700x413.jpg',
     atributos:{
-        ataque: 80,
+        ataque: 77,
         defesa: 25,
-        magia: 30
+        magia: 36
     } 
 }
 let carta4 = {
@@ -30,26 +30,26 @@ let carta4 = {
     foto: "https://i.imgur.com/018AGPn.jpg",
     atributos:{
         ataque: 30,
-        defesa: 70,
-        magia: 70
+        defesa: 66,
+        magia: 71
     }    
 }
 let carta5 = {
     nome: 'Nasus',
     foto: 'https://i.imgur.com/A1ndkwi.jpg',
     atributos:{
-        ataque: 70,
-        defesa: 75,
-        magia: 30
+        ataque: 58,
+        defesa: 62,
+        magia: 36
     } 
 }
 let carta6 = {
     nome: 'Shen',
     foto: 'https://i.imgur.com/UqTL4jF.jpg',
     atributos:{
-        ataque: 45,
-        defesa: 80,
-        magia: 35
+        ataque: 44,
+        defesa: 72,
+        magia: 39
     } 
 }
 
@@ -57,9 +57,9 @@ let carta7 = {
     nome: 'Elise',
     foto: 'https://i.imgur.com/t360nLi.jpg',
     atributos:{
-        ataque: 35,
-        defesa: 50,
-        magia: 70
+        ataque: 30,
+        defesa: 55,
+        magia: 65
     } 
 }
 
@@ -68,8 +68,8 @@ let carta8 = {
     foto: 'https://i.imgur.com/08qYdgb.jpg',
     atributos:{
         ataque: 30,
-        defesa: 40,
-        magia: 75
+        defesa: 42,
+        magia: 72
     } 
 }
 
@@ -77,9 +77,9 @@ let carta9 = {
     nome: 'Rammus',
     foto: 'https://i.imgur.com/cnRyvcO.jpg',
     atributos:{
-        ataque: 35,
-        defesa: 80,
-        magia: 45
+        ataque: 30,
+        defesa: 70,
+        magia: 40
     } 
 }
 
@@ -87,13 +87,69 @@ let carta10 = {
     nome: 'Riven',
     foto: 'https://i.imgur.com/RggK2e3.jpg',
     atributos:{
-        ataque: 90,
-        defesa: 55,
-        magia: 15
+        ataque: 70,
+        defesa: 34,
+        magia: 18
     } 
 }
 
-let baralho = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10]
+let carta11 = {
+    nome: 'Akali',
+    foto: 'https://i.imgur.com/H6j8pyl.jpg',
+    atributos:{
+        ataque: 62,
+        defesa: 40,
+        magia: 69
+    } 
+}
+
+let carta12 = {
+    nome: 'Nidalee',
+    foto: 'https://i.imgur.com/6vSDEI1.jpg',
+    atributos:{
+        ataque: 42,
+        defesa: 35,
+        magia: 55
+    } 
+}
+
+let carta13 = {
+    nome: 'Swain',
+    foto: 'https://i.imgur.com/lmQRbmz.jpg',
+    atributos:{
+        ataque: 10,
+        defesa: 44,
+        magia: 69
+    } 
+}
+
+let carta14 = {
+    nome: 'Viego',
+    foto: 'https://i.imgur.com/h4qMnNW.jpg',
+    atributos:{
+        ataque: 70,
+        defesa: 40,
+        magia: 10
+    } 
+}
+
+let baralho = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10, carta11, carta12, carta13, carta14]
+let meuBaralho = []
+
+function distribuiCartas(){
+    for (let minhasCartas = 0; minhasCartas < 7; minhasCartas++){
+        let indiceAleatorio = Math.floor(Math.random() * baralho.length)
+
+        if(indiceAleatorio >= baralho.length && indiceAleatorio >= baralho.length){
+        indiceAleatorio = (baralho.length-1)
+        }
+
+        meuBaralho.push(baralho.splice(indiceAleatorio, 1)[0]);
+        
+    }
+}
+
+distribuiCartas()
 
 let cartaMaquina
 let cartaJogador
@@ -102,45 +158,40 @@ let campoJogador = document.querySelector('.cartaJogador')
 let campoMaquina = document.querySelector('.cartaMaquina')
 let pontos = document.querySelector('.pontuacao')
 let versus = document.querySelector('.versus')
-let ptsJogador = 0
-let ptsMaquina = 0
 
-pontos.innerHTML = `Jogador - ${ptsJogador} | ${ptsMaquina} - Máquina `
+pontos.innerHTML = `Jogador possui ${meuBaralho.length} cartas | Máquina possui ${baralho.length} cartas`
 
-function sortearCarta(){
-    numeroCartaMaquina = Math.floor(Math.random() * 10)
-    numeroCartaJogador = Math.floor(Math.random() * 10)
 
-    if(numeroCartaMaquina > 10 && numeroCartaJogador > 10){
-        numeroCartaMaquina = 9
-    }
-    cartaMaquina = baralho[numeroCartaMaquina]
-   
-    while (numeroCartaMaquina == numeroCartaJogador) {
-        numeroCartaJogador = Math.floor(Math.random() * 10)
-    }
+function sortearCarta(){    
+        numeroCartaMaquina = Math.floor(Math.random() * baralho.length) 
+        numeroCartaJogador = Math.floor(Math.random() * meuBaralho.length)
 
-    cartaJogador = baralho[numeroCartaJogador]
-    document.getElementById('campoBatalha').style.display="none"
-    document.getElementById('btnSortear').style.display="none"
-    document.getElementById('btnJogar').disabled = false
-    document.getElementById('escolhaAtributo').style.display="block"
+        if(numeroCartaMaquina >= baralho.length && numeroCartaJogador >= meuBaralho.length){
+            numeroCartaMaquina = (baralho.length-1)
+        }
+        cartaMaquina = baralho[numeroCartaMaquina]
 
-    minhaCarta.style.display = 'block'
+        cartaJogador = meuBaralho[numeroCartaJogador]
+        document.getElementById('campoBatalha').style.display="none"
+        document.getElementById('btnSortear').style.display="none"
+        document.getElementById('btnJogar').disabled = false
+        document.getElementById('escolhaAtributo').style.display="block"
 
-    minhaCarta.innerHTML = `
-    <div class='carta'>
-    <div class="nomeCarta">${cartaJogador.nome}</div>
-    <img class="imgCarta" src="${cartaJogador.foto}" alt="">             
-    <div class="atributosCarta">
-    <div class="bg1"><span>Ataque</span><span>${cartaJogador.atributos.ataque}</span></div>
-    <div class="bg2"><span>Defesa</span><span>${cartaJogador.atributos.defesa}</span></div>
-    <div class="bg1"><span>Magia</span><span>${cartaJogador.atributos.magia}</span></div>
-    </div></div>`                           
+        minhaCarta.style.display = 'block'
 
-     exibirOpcoes()
-     
+        minhaCarta.innerHTML = `
+        <div class='carta'>
+        <div class="nomeCarta">${cartaJogador.nome}</div>
+        <img class="imgCarta" src="${cartaJogador.foto}" alt="">             
+        <div class="atributosCarta">
+        <div class="bg1"><span>Ataque</span><span>${cartaJogador.atributos.ataque}</span></div>
+        <div class="bg2"><span>Defesa</span><span>${cartaJogador.atributos.defesa}</span></div>
+        <div class="bg1"><span>Magia</span><span>${cartaJogador.atributos.magia}</span></div>
+        </div></div>` 
+        exibirOpcoes()
+                           
 }
+
 
 function exibirOpcoes(){
     let opcoes = document.getElementById('opcoes')
@@ -189,18 +240,14 @@ function jogar(){
     </div></div>`
     
     if(cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]){
-        console.log(`Jogador: ${cartaJogador.atributos[atributoSelecionado]}`)
-        console.log(`Máquina: ${cartaMaquina.atributos[atributoSelecionado]}`)
         resultado.innerHTML = 'Parabéns, você ganhou!'
-        ptsJogador++
+        meuBaralho.push
+        meuBaralho.push(baralho.splice(numeroCartaMaquina, 1)[0]);
+
     } else if(cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]){
-        console.log(`Jogador: ${cartaJogador.atributos[atributoSelecionado]}`)
-        console.log(`Máquina: ${cartaMaquina.atributos[atributoSelecionado]}`)
         resultado.innerHTML = 'Você perdeu!'
-        ptsMaquina++
+        baralho.push(meuBaralho.splice(numeroCartaJogador, 1)[0]);
     } else{
-        console.log(`Jogador: ${cartaJogador.atributos[atributoSelecionado]}`)
-        console.log(`Máquina: ${cartaMaquina.atributos[atributoSelecionado]}`)
         resultado.innerHTML = 'Empate'
     }
     
@@ -208,17 +255,56 @@ function jogar(){
     document.getElementById('escolhaAtributo').style.display="none"
     document.getElementById('btnJogar').disabled = true
     document.getElementById('btnNovoJogo').disabled = false
-    pontos.innerHTML = `Jogador - ${ptsJogador} | ${ptsMaquina} - Máquina `
+    pontos.innerHTML = `Jogador possui ${meuBaralho.length} cartas | Máquina possui ${baralho.length} cartas`
 }
 
+
 function novoJogo(){
-    document.getElementById('btnNovoJogo').disabled = true
-    document.getElementById('btnSortear').disabled = false
-    minhaCarta.style.display = "none"
-    document.getElementById('escolhaAtributo').style.display="none"
-    document.getElementById('btnSortear').style.display="inline-block"
+
+    if(baralho.length == 0){
+        resultado.innerHTML = `As cartas do seu oponente acabaram, e você ganhou o jogo.<br> Parabéns!`
+        baralho = meuBaralho
+        escodeButton()
+    }else if(meuBaralho.length == 0){
+        resultado.innerHTML = `As cartas acabaram, e você perdeu o jogo.<br> Boa sorte na próxima vez`
+        escodeButton()
+    }else{
+        document.getElementById('btnNovoJogo').disabled = true
+        minhaCarta.style.display = "none"
+        document.getElementById('escolhaAtributo').style.display="none" 
+        campoJogador.innerHTML = ''
+        versus.innerHTML = ''
+        campoMaquina.innerHTML = ''
+        resultado.innerHTML = ''
+        sortearCarta()
+        pontos.innerHTML = `Jogador possui ${meuBaralho.length} cartas | Máquina possui ${baralho.length} cartas`
+    }
+}
+
+function reiniciarJogo(){
+    meuBaralho = []
+    distribuiCartas()
+    pontos.innerHTML = `Jogador possui ${meuBaralho.length} cartas | Máquina possui ${baralho.length} cartas`
+    resultado.innerHTML = ''
+    mostraButton()
+
+}
+
+function escodeButton(){
+    document.getElementById('reiniciarJogo').style.display="inline-block"
+    document.getElementById('btnJogar').style.display="none"
+    document.getElementById('btnNovoJogo').style.display="none" 
     campoJogador.innerHTML = ''
     versus.innerHTML = ''
     campoMaquina.innerHTML = ''
-    resultado.innerHTML = ''
+
+}
+
+function mostraButton(){
+    document.getElementById('reiniciarJogo').style.display="none"
+    document.getElementById('btnJogar').style.display="inline-block"
+    document.getElementById('btnNovoJogo').style.display="inline-block" 
+    document.getElementById('btnSortear').disabled = false
+    document.getElementById('btnSortear').style.display="inline-block"
+    document.getElementById('reiniciarJogo').style.display="none" 
 }
